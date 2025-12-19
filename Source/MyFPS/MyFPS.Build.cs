f32a1799ba13f0268f84d0f082cdffe99664e456
Cui Haoran <2585941008@qq.com>
2025-12-19 12:50:48 +0800

    Initial Commit

---
+++ Source/MyFPS/MyFPS.Build.cs
@@ -0,0 +1,44 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class MyFPS : ModuleRules
+{
+	public MyFPS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"MyFPS",
+			"MyFPS/Variant_Horror",
+			"MyFPS/Variant_Horror/UI",
+			"MyFPS/Variant_Shooter",
+			"MyFPS/Variant_Shooter/AI",
+			"MyFPS/Variant_Shooter/UI",
+			"MyFPS/Variant_Shooter/Weapons"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/MyFPS/MyFPS.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyFPS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyFPS, "MyFPS" );
+
+DEFINE_LOG_CATEGORY(LogMyFPS)
